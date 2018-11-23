ead8a4bb35fb5d721dfecbf21a45f489fe2971c1
Daniel Kang <danielkang674@gmail.com>
2018-11-23 02:24:58 -0600

    Pseudoclassical constructor with prototypes

---
+++ prototypePattern.js
@@ -0,0 +1,15 @@
+function Car(model, year, miles) {
+  this.model = model;
+  this.year = year;
+  this.miles = miles;
+};
+
+Car.prototype.toString = function () {
+  return `${this.model} has done ${this.miles} miles`
+};
+
+const civic = new Car('Honda Civic', 1991, 90000);
+const corolla = new Car('Toyota Corolla', 2001, 50000);
+
+console.log(civic.toString());
+console.log(corolla.toString());
