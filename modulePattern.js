a1671c634da79c5e700e1cc6b2fbdd31bed78027
Daniel Kang <danielkang674@gmail.com>
2018-11-23 17:00:05 -0600

    Module pattern es6 refactor

--- modulePattern.js
+++ modulePattern.js
@@ -34,3 +34,64 @@ const myNamespace = (function () {
 })();
 
 myNamespace.myPublicFunction('yes');
+
+const basketModule = function () {
+  // private
+  let basket = [];
+  const doSomethingPrivate = () => {
+    console.log('private');
+  };
+  // public object
+  return {
+    addItem(item) {
+      basket.push(item);
+    },
+    getItemCount() {
+      return basket.length;
+    },
+    doSomething: doSomethingPrivate,
+    getTotal() {
+      let quantity = this.getItemCount(), price = 0;
+      while (quantity--) {
+        price += basket[quantity].price;
+      }
+      return price;
+    },
+  }
+};
+
+const basket = basketModule();
+basket.addItem({
+  item: 'bread',
+  price: 0.5
+});
+basket.addItem({
+  item: 'butter',
+  price: 0.3
+});
+console.log(basket.getItemCount());
+console.log(basket.getTotal());
+basket.doSomething();
+console.log(basket.basket); // undefined cuz private
+
+const RevealingModule = () => {
+  let privateVar = 'John Doe', publicGreeting = 'hello';
+
+  const privateFunc = () => {
+    console.log(`Name ${privateVar}`);
+  };
+  const publicSetName = (strName) => {
+    privateVar = strName;
+  };
+  const publicGetName = () => {
+    privateFunc();
+  };
+
+  return { publicSetName, publicGreeting, publicGetName };
+};
+
+const revealing = RevealingModule();
+revealing.publicGetName();
+revealing.publicSetName('Dankey Kang');
+console.log(revealing.publicGreeting);
+
